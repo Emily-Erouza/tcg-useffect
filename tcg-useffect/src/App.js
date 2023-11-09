dba595c374d951beb2c77b2509196ec1d3ecbaa5
Emily-Erouza <emilysbongile17@gmail.com>
2023-11-09 10:24:09 -0500

    add tcg-useffect

--- tcg-useffect/src/App.js
+++ tcg-useffect/src/App.js
@@ -1,16 +1,37 @@
-import { useEffect,useState } from 'react';
-import './App.css';
+// import { useEffect,useState } from 'react';
+// import './App.css';
 
-function App() {
-  const [count, setCount] = useState(0);
+// function App() {
+//   const [count, setCount] = useState(0);
 
-  useEffect(() => {
-    setTimeout(() => {
-      setCount((count) => count + 1);
-    }, 1000);
-  });
+//   useEffect(() => {
+//     setTimeout(() => {
+//       setCount((count) => count + 1);
+//     }, 1000);
+//   });
 
-  return <h1>I've rendered {count} times!</h1>;
+//   return <h1>I've rendered {count} times!</h1>;
+
+
+  import { useState } from "react";
+import ReactDOM from "react-dom/client";
+
+function FavoriteColor() {
+  const [color, setColor] = useState("red");
+
+  return (
+    <>
+      <h1>My favorite color is {color}!</h1>
+      <button
+        type="button"
+        onClick={() => setColor("blue")}
+      >Blue</button>
+    </>
+  )
 }
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<FavoriteColor />);
+
 
 export default App;
